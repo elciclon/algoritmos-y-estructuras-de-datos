c92f70f218941f63d7c1185b86286d05be5458f1
Tu Nombre <you@example.com>
2025-04-07 13:47:19 -0300

    Extrae digitos

--- digitoUnidades .hs
+++ digitoUnidades .hs
@@ -1,2 +1,8 @@
 digitoUnidades :: Int -> Int
 digitoUnidades x = mod x 10
+
+digitoDecenas :: Int -> Int
+digitoDecenas x = div (mod x 100) 10
+
+digitoGeneral :: Int -> Int
+digitoGeneral x = div (mod x 10¨(i+1)) 10"i
