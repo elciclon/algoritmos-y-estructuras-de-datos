15936d54417e6337ef8cdb7cc12e3a89d67e2f71
Tu Nombre <you@example.com>
2025-04-07 13:55:38 -0300

    ampliado

--- todoMenor.hs
+++ todoMenor.hs
@@ -1,3 +1,5 @@
 todoMenor :: (Float, Float) -> (Float,Float) -> Bool
-
 todoMenor(a,b) (c,d) = a < c && b < d
+
+todoMenor :: (Float, Float) -> (Float,Float) -> Bool
+todoMenor2 x y = fst(x)  < fst(y) && snd(x) < snd(y)
