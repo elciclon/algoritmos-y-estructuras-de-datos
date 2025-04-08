8e4f9d61edf44ff8c9d57e19adfe3991605e6c92
elciclon <33224715+elciclon@users.noreply.github.com>
2025-04-08 14:41:15 -0300

    Algunos ejercicios

--- maximo3.hs
+++ maximo3.hs
@@ -1,7 +1,7 @@
-maximo3 :: Int -> Int-> Int -> Int
+maximo3 :: Int -> Int -> Int -> Int
 maximo3 x y z = maximo2 (maximo2 x y) z
 
-maximo2 :: Int -> Int-> Int
-maximo2 x y | y > x = y
-            | otherwise = x
-
+maximo2 :: Int -> Int -> Int
+maximo2 x y
+  | y > x = y
+  | otherwise = x
