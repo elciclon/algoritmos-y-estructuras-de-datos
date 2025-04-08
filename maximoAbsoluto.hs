9cfe7e915618b8c1fb8b4c441c8fa8f777cfd367
elciclon <33224715+elciclon@users.noreply.github.com>
2025-04-08 14:17:27 -0300

    maximoAbsoluto

---
+++ maximoAbsoluto.hs
@@ -0,0 +1,10 @@
+-- maximoAbsoluto: devuelve el maximo entre el valor absoluto de dos numeros enteros
+maximoAbsoluto :: Int -> Int -> Int
+maximoAbsoluto x y
+  | absoluto y > absoluto x = absoluto y
+  | otherwise = absoluto x
+
+absoluto :: Int -> Int
+absoluto x
+  | x < 0 = -x
+  | otherwise = x
