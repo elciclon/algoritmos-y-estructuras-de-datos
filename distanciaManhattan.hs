752932c21a962512a16a6cf759c23d6be4b8d4b6
elciclon <elciclon>
2025-04-09 13:10:26 -0300

    Suma y compara digitos

--- distanciaManhattan.hs
+++ distanciaManhattan.hs
@@ -1,4 +1,6 @@
-distanciaManhattan :: (Float, Float, Float) -> (Float, Float, Float) -> Float
+type Coordenada3D = (Float, Float, Float)
+
+distanciaManhattan :: Coordenada3D -> Coordenada3D -> Float
 distanciaManhattan (x1, x2, x3) (y1, y2, y3) = absoluto(x1 - y1) + absoluto(x2 - y2) + absoluto(x3 - y3) 
 
 absoluto :: Float -> Float
