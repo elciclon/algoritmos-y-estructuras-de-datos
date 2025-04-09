ba4745d8e98d8abeeb6505a04a9255269b739710
elciclon <elciclon>
2025-04-09 12:21:03 -0300

    Elige pares

---
+++ posPrimerPar.hs
@@ -0,0 +1,8 @@
+posPrimerPar :: Int -> Int -> Int -> Int
+posPrimerPar (x, y, z) | esPar x = 1
+                       | esPar y = 2
+                       | esPar z = 3
+                       | otherwise = 4
+
+esPar :: Int -> Bool
+esPar x = mod x 2 == 0 = True
