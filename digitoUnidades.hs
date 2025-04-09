d64939469d7cea1fc03792c3fd9f6f8afd8b2a99
elciclon <33224715+elciclon@users.noreply.github.com>
2025-04-09 09:41:51 -0300

    Extrae dígitos

--- digitoUnidades.hs
+++ digitoUnidades.hs
@@ -1,3 +1,9 @@
 -- digitoUnidades: dado un número entero, extrae su dígito de las unidades
 digitoUnidades :: Int -> Int
 digitoUnidades x = mod x 10
+
+digitoDecenas :: Int -> Int
+digitoDecenas x = div (mod x 100) 10
+
+digitoGeneral :: Int -> Int -> Int
+digitoGeneral x i = div (mod x (10 ^ (i + 1))) (10 ^ i)
