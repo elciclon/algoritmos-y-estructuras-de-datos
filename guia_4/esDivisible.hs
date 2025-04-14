fefaf3fd95627360ca3fe5f0634c7bd667e61570
elciclon <33224715+elciclon@users.noreply.github.com>
2025-04-14 09:20:42 -0300

    Implementado esDivisible

--- guia_4/esDivisible.hs
+++ guia_4/esDivisible.hs
@@ -1,6 +1,18 @@
-{-Ejercicio 3. Especificar e implementar la funcion esDivisible :: Integer ->Integer ->Bool que dados dos n´umeros
-naturales determinar si el primero es divisible por el segundo. No está permitido utilizar las funciones mod ni div.-}
+{-Ejercicio 3. Especificar e implementar la funcion esDivisible :: Integer ->Integer ->Bool que dados dos numeros
+naturales determinar si el primero es divisible por el segundo. No está permitido utilizar las funciones mod ni div.
+{problema esDivisible{a:N, b:N}:Bool
+                        requiere:{True}
+                        asegura: {a es divisible por b -> res = true}
+                                 {a no es divisible por b -> res = false}
+                        }
+                        -}
+
 esDivisible :: Integer -> Integer -> Bool
 esDivisible a b
-  | a < b && a == 0 = True
-  | a < b && a /= 0 = False
+  | resto a b == 0 = True
+  | otherwise = False
+
+resto :: Integer -> Integer -> Integer
+resto a b
+  | a < b = a
+  | otherwise = resto (a - b) b
