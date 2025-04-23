db859481e0657478c3540db5463d7c1a410cf14b
elciclon <33224715+elciclon@users.noreply.github.com>
2025-04-23 19:45:42 -0300

    Implementado sonCoprimos

--- guia_4/sonCoprimos.hs
+++ guia_4/sonCoprimos.hs
@@ -1,8 +1,11 @@
--- sonCoprimos :: Integer -> Integer -> Bool
-esMenor :: Integer -> Integer -> Bool
-esMenor a b = a < b
+sonCoprimos :: Integer -> Integer -> Bool
+sonCoprimos a b
+  | a == b = False
+  | a < b = divisoresDe a b a
+  | otherwise = divisoresDe b a b
 
-esDivisor :: Integer -> Integer -> Bool
-esDivisor n i
-  | mod n i == 0 = True
-  | otherwise = False
+divisoresDe :: Integer -> Integer -> Integer -> Bool
+divisoresDe _ _ 1 = True
+divisoresDe x y z
+  | mod x z == 0 && mod y z == 0 = False
+  | otherwise = divisoresDe x y (z - 1)
