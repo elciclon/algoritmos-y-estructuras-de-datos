4e828dfe8deb7a25b70494a815b3c407a2232aa4
elciclon <33224715+elciclon@users.noreply.github.com>
2025-04-22 21:02:41 -0300

    Consultar por el problema de tipo

---
+++ guia_5/eAprox.hs
@@ -0,0 +1,17 @@
+{-Especificar e implementar una función eAprox :: Integer ->Float que aproxime el valor del número e
+a partir de la siguiente sumatoria:
+eˆ(n) = sumatoria desde i=0 hasta n de 1/i!
+i
+problema eAprox(n:N): R {
+    requiere:{True}
+    asegura:{resultado=valor aproximado de n}
+}
+-}
+eAprox :: Integer -> Float
+eAprox 0 = 1
+eAprox n = (1 / (fromInteger (factorial n)) + eAprox (n - 1))
+
+factorial :: Integer -> Integer
+factorial n
+  | n == 1 = 1
+  | otherwise = n * factorial (n - 1)
