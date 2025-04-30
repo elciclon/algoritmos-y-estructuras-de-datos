f33de207d64080d7326d64700496cf2b80c32558
wendy <sfsfjsf>
2025-04-30 11:28:24 -0300

    Implementando contar palabras

--- guia_5/contarPalabras.hs
+++ guia_5/contarPalabras.hs
@@ -29,7 +29,3 @@ borrarUltimoBlanco [] = []
 borrarUltimoBlanco [' '] = []
 borrarUltimoBlanco (x : xs) = x : borrarUltimoBlanco xs
 
-{-longitud :: [t] -> Integer, que dada una lista devuelve su cantidad de elementos.-}
-longitud :: [t] -> Integer
-longitud [] = 0
-longitud (_ : xs) = 1 + longitud xs
