d8d2e670929bb4d85de9a2e39af81b5963f5d00e
elciclon <33224715+elciclon@users.noreply.github.com>
2025-05-01 16:23:36 -0300

    generarStock

--- ejercicios_integradores/generarStock.hs
+++ ejercicios_integradores/generarStock.hs
@@ -1,17 +1,31 @@
---["madera", "clavo", "pala", "clavo"]
---[("madera", 1),("clavo",2),("pala",1)]
+-- ["madera", "clavo", "pala", "clavo"]
+-- [("madera", 1),("clavo",2),("pala",1)]
 
---generarStock :: [String] -> [(String, Int)]
+-- generarStock :: [String] -> [(String, Int)]
+-- generarStock mercaderia | head filtrarRepetidos mercaderia
+
+crearTuplas :: String -> [String] -> (String, Int)
+crearTuplas producto mercaderia = (producto, contarProducto producto mercaderia)
+
+contarProducto :: String -> [String] -> Int
+contarProducto producto (x : [])
+  | producto == x = 1
+  | otherwise = 0
+contarProducto producto mercaderia
+  | producto == head mercaderia = 1 + contarProducto producto (tail mercaderia)
+  | otherwise = contarProducto producto (tail mercaderia)
 
 filtrarRepetidos :: [String] -> [String]
-filtrarRepetidos (x:[]) = [x] 
-filtrarRepetidos (pal:xs) | pertenece pal xs == True = pal: quitar pal xs
-                          | otherwise = pal: filtrarRepetidos xs
+filtrarRepetidos (x : []) = [x]
+filtrarRepetidos (pal : xs)
+  | pertenece pal xs == True = pal : quitar pal xs
+  | otherwise = pal : filtrarRepetidos xs
 
 quitar :: String -> [String] -> [String]
-quitar palabra (_:[]) = []
-quitar palabra (pal:xs) | palabra == pal = quitar palabra xs
-                        | otherwise = pal: quitar palabra xs
+quitar palabra (_ : []) = []
+quitar palabra (pal : xs)
+  | palabra == pal = quitar palabra xs
+  | otherwise = pal : quitar palabra xs
 
 pertenece :: String -> [String] -> Bool
 pertenece _ [] = False
