a1cb3b556a41031d70617bbc3500c20017e81c5f
elciclon <33224715+elciclon@users.noreply.github.com>
2025-05-21 19:32:48 -0300

    Implementando pos secuencia ordenada mas larga

--- guia_7/pos_secuencia_ordenada_mas_larga.py
+++ guia_7/pos_secuencia_ordenada_mas_larga.py
@@ -1,5 +1,9 @@
-def pos_secuencia_ordenada_mas_larga(s:list[int]) -> int:
+def pos_secuencia_ordenada_mas_larga(s: list[int]) -> int:
     indice: int = 0
     secuencia_ordenada: list[int] = []
     secuencia_ordenada_mas_larg: list[int] = []
     for i in range(0, len(s)):
+        if s[i] >= s[i + 1]:
+            continue
+        else:
+            secuencia_ordenada.append(s[i])
