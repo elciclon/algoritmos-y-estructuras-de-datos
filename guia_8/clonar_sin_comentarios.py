fc79317fca888386821a69ce699e22412adb22d6
wendy <sfsfjsf>
2025-05-28 13:55:51 -0300

    clonar sin comentarios final

--- guia_8/clonar_sin_comentarios.py
+++ guia_8/clonar_sin_comentarios.py
@@ -2,7 +2,7 @@ from typing import TextIO
 
 def clonar_sin_comentarios(nombre_archivo_entrada: str, nombre_archivo_salida: str):
     archivo_entrada: TextIO = open(nombre_archivo_entrada, 'r', encoding="utf-8")     
-    archivo_salida: TextIO = open(nombre_archivo_entrada, 'w', encoding="utf-8")     
+    archivo_salida: TextIO = open(nombre_archivo_salida, 'w', encoding="utf-8")     
     for linea in archivo_entrada.readlines():
         for char in linea:
             if char != ' ' and char != '#':
@@ -10,3 +10,4 @@ def clonar_sin_comentarios(nombre_archivo_entrada: str, nombre_archivo_salida: s
 
     archivo_entrada.close()
     archivo_salida.close()
+
