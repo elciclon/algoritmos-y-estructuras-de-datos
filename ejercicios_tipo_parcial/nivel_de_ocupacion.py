c9a58c9fb1c66ea948ee7b38e620c0ead08f9b40
wendy <sfsfjsf>
2025-06-04 12:10:03 -0300

    Implementado con tests

---
+++ ejercicios_tipo_parcial/nivel_de_ocupacion.py
@@ -0,0 +1,12 @@
+def nivel_de_ocupacion(camas_por_piso: list[list[bool]]) -> list[float]:
+    promedio_por_piso: list[float] = []
+    for piso in camas_por_piso:
+        cantidad_de_camas: int = 0
+        suma_de_ocupadas: int = 0
+        for cama in piso:
+            cantidad_de_camas += 1
+            if cama == True:
+                suma_de_ocupadas += 1
+        promedio_por_piso.append(suma_de_ocupadas / cantidad_de_camas)
+
+    return promedio_por_piso
