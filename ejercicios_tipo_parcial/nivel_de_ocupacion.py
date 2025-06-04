e7e2ef9cd262f2b707749a715cfdea65db697686
wendy <sfsfjsf>
2025-06-04 12:19:11 -0300

    Refactoring

--- ejercicios_tipo_parcial/nivel_de_ocupacion.py
+++ ejercicios_tipo_parcial/nivel_de_ocupacion.py
@@ -5,7 +5,7 @@ def nivel_de_ocupacion(camas_por_piso: list[list[bool]]) -> list[float]:
         suma_de_ocupadas: int = 0
         for cama in piso:
             cantidad_de_camas += 1
-            if cama == True:
+            if cama:
                 suma_de_ocupadas += 1
         promedio_por_piso.append(suma_de_ocupadas / cantidad_de_camas)
 
--- ejercicios_tipo_parcial/test_ejercicios_tipo_parcial.py
+++ ejercicios_tipo_parcial/test_ejercicios_tipo_parcial.py
@@ -3,7 +3,7 @@ from nivel_de_ocupacion import *
 
 class Nivel_de_ocupacionTest(unittest.TestCase):
     def test_nivel_de_ocupacion(self):
-        self.assertEqual(nivel_de_ocupacion([[True, False, False], [False,False,True], [True, True, True]]), [2/3, 1/3, 1.0])
+        self.assertEqual(nivel_de_ocupacion([[True, False, True],[False,False,True], [True, True, True]]), [2/3, 1/3, 1.0])
 
 if __name__ == "__main__":
     unittest.main(verbosity = 2)
