6030d249ea25e080b0a1b5f25d645d2e95b986fd
elciclon <33224715+elciclon@users.noreply.github.com>
2025-06-11 09:01:50 -0300

    inventario implementado

--- guia_8/inventario.py
+++ guia_8/inventario.py
@@ -13,7 +13,24 @@ def actualizar_stock(
     inventario[nombre]["cantidad"] = cantidad
 
 
+def actualizar_precio(
+    inventario: dict[str, dict[str, int | float]], nombre: str, precio: float
+) -> None:
+    inventario[nombre]["precio"] = precio
+
+
+def calcular_valor_inventario(inventario: dict[str, dict[str, int | float]]) -> float:
+    valor_inventario: float = 0
+    for item in inventario.values():
+        valor_inventario += item["precio"] * item["cantidad"]
+    return valor_inventario
+
+
 inventario = {}
 agregar_producto(inventario, "Camisa", 20.0, 50)
 agregar_producto(inventario, "Pantalón", 30.0, 30)
 actualizar_stock(inventario, "Camisa", 10)
+actualizar_precio(inventario, "Camisa", 30.0)
+actualizar_precio(inventario, "Camisa", 20.0)
+valor_total: float = calcular_valor_inventario(inventario)
+print(valor_total)
