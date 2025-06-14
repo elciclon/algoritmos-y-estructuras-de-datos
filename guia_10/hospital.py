82dcc642a90d72cb6d79fe9e9639b4943c8b1cea
elciclon <33224715+elciclon@users.noreply.github.com>
2025-06-14 16:23:26 -0300

    Corregidas copias de colas

--- guia_10/hospital.py
+++ guia_10/hospital.py
@@ -1,10 +1,13 @@
 from queue import Queue as Cola
+from copiar_cola import *
 
 
 def orden_de_atencion(urgentes: Cola[int], postergables: Cola[int]) -> Cola[int]:
     orden: Cola[int] = Cola()
+    copy_urgentes: Cola[int] = copiar_cola(urgentes)
+    copy_postergables: Cola[int] = copiar_cola(postergables)
     while not urgentes.empty():
-        orden.put(urgentes.get())
-        orden.put(postergables.get())
+        orden.put(copy_urgentes.get())
+        orden.put(copy_postergables.get())
 
     return orden
