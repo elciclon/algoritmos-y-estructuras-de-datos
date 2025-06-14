0348e5501aea3249cabc2126cd77919617289b54
elciclon <33224715+elciclon@users.noreply.github.com>
2025-06-14 16:23:35 -0300

    hospital

--- guia_10/hospital.py
+++ guia_10/hospital.py
@@ -6,7 +6,7 @@ def orden_de_atencion(urgentes: Cola[int], postergables: Cola[int]) -> Cola[int]
     orden: Cola[int] = Cola()
     copy_urgentes: Cola[int] = copiar_cola(urgentes)
     copy_postergables: Cola[int] = copiar_cola(postergables)
-    while not urgentes.empty():
+    while not copy_urgentes.empty():
         orden.put(copy_urgentes.get())
         orden.put(copy_postergables.get())
 
